bastelfreak/ezbake

commit 9768a77de95ff19f66f4b328fc60efe69f5f3a6b

Use virtual Java packages on Red Hat and set java_bin
This uses the virtual packages jre-VERSION-headless instead of
explicitly openjdk. This allows other JREs to provide the same.

It also uses an explicit path to the JRE specific java bin. At least on
EL9 this allows the following upgrade path to work:

    dnf install https://yum.puppet.com/puppet7-release-el-9.noarch.rpm
    dnf install puppetserver
    dnf install https://yum.puppet.com/puppet8-release-el-9.noarch.rpm
    dnf upgrade puppetserver

Prior to this patch it would break, because it used /usr/bin/java which
will point to Java 8. By using /usr/lib/jvm/jre-17/bin/java we know for
sure it is Java 17.

resources/puppetlabs/lein-ezbake/template/global/ext/fpm.rb
@@ -168,9 +168,11 @@ if options.output_type == 'rpm'
     options.systemd_el = 1
   elsif options.operating_system == :el
     if options.os_version == 7
-      options.java = 'java-11-openjdk-headless'
+      options.java = 'jre-11-headless'
+      options.java_bin = '/usr/lib/jvm/jre-11/bin/java'
     elsif options.os_version >= 8
-      options.java = 'java-17-openjdk-headless'
+      options.java = 'jre-17-headless'
+      options.java_bin = '/usr/lib/jvm/jre-17/bin/java'
     else
       fail "Unrecognized el os version #{options.os_version}"
     end

commit 8dcb1371b1d4931c6ccdd27f6d20580feb9e84ea

Patch java_bin if overridden
In 9768a77de95ff19f66f4b328fc60efe69f5f3a6b the java binary was
overridden, but this didn't have a point because it's not available when
the files are rendered. They only have access to the ezbake config.

This takes an approach of patching the files after the fact. It writes
out backup files that are restored after. That is to deal with the fact
that our build process isn't clean and is reused between multiple
builds.

Fixes: 9768a77de95f ("Use virtual Java packages on Red Hat and set java_bin")

resources/puppetlabs/lein-ezbake/template/global/ext/fpm.rb
@@ -4,6 +4,37 @@ require 'open3'
 require 'optparse'
 require 'ostruct'
 
+def patch_files(options)
+  if options.java_bin == EZBake::Config[:java_bin]
+    yield
+  else
+    suffix = '.backup'
+    [
+      # Debian
+      '/etc/default/puppet*',
+      '/lib/systemd/system/puppet*.service',
+      # RPM
+      '/usr/lib/systemd/system/puppet*.service',
+      '/etc/sysconfig/puppet*',
+    ].each do |path|
+      Dir.glob(File.join(options.chdir, path)).each do |real_path|
+        content = File.read(real_path)
+        if content.include?(EZBake::Config[:java_bin])
+          FileUtils.cp(real_path, "#{real_path}#{suffix}")
+          File.write(real_path, content.gsub(EZBake::Config[:java_bin], options.java_bin))
+        end
+      end
+    end
+
+    yield
+
+    Dir.glob(File.join(options.chdir, '**', "*#{suffix}")).each do |path|
+      target = File.join(File.dirname(path), File.basename(path, suffix))
+      FileUtils.mv(path, target)
+    end
+  end
+end
+
 options = OpenStruct.new
 
 # ezbake.rb is rendered from
@@ -432,22 +463,24 @@ if options.debug
   puts "#{Dir.pwd}"
 end
 
-# fpm sends all output to stdout
-out, _, stat = Open3.capture3("#{fpm_editor} fpm #{fpm_opts.join(' ')}")
-fail "Error trying to run FPM for #{options.dist}!\n#{out}" unless stat.success?
-
-puts "#{out}"
-
-if options.termini
-  if options.debug
-    puts "=========================="
-    puts "FPM COMMAND"
-    puts "fpm #{termini_opts.join(' ')}"
-    puts "=========================="
-  end
-
+patch_files(options) do
   # fpm sends all output to stdout
-  out, _, stat = Open3.capture3("fpm #{termini_opts.join(' ')}")
-  fail "Error trying to run FPM for the termini for #{options.dist}!\n#{out}" unless stat.success?
+  out, _, stat = Open3.capture3("#{fpm_editor} fpm #{fpm_opts.join(' ')}")
+  fail "Error trying to run FPM for #{options.dist}!\n#{out}" unless stat.success?
+
   puts "#{out}"
+
+  if options.termini
+    if options.debug
+      puts "=========================="
+      puts "FPM COMMAND"
+      puts "fpm #{termini_opts.join(' ')}"
+      puts "=========================="
+    end
+
+    # fpm sends all output to stdout
+    out, _, stat = Open3.capture3("fpm #{termini_opts.join(' ')}")
+    fail "Error trying to run FPM for the termini for #{options.dist}!\n#{out}" unless stat.success?
+    puts "#{out}"
+  end
 end